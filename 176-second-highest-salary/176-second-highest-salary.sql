67dc1cf3a5eef510a448d14b3a47c24a2452139c
Baplisca <65814732+Baplisca@users.noreply.github.com>
2022-09-04 17:46:20 +0900

    Time: 376 ms (26.94%), Space: 0B (100.00%) - LeetHub

---
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -0,0 +1,14 @@
+select
+    (
+        select 
+            salary
+        from
+            Employee
+        group by
+            salary
+        order by 
+            salary desc
+        limit 1 offset 1
+    ) 
+as SecondHighestSalary
+;
