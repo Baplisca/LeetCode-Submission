115c3417b0c3858a67bb186c5f7e7320a727d624
Baplisca <65814732+Baplisca@users.noreply.github.com>
2022-09-29 19:47:45 +0900

    Time: 937 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
@@ -1,51 +1,16 @@
 select
-    *
-from
-(select
-    s1.id,
-    s1.visit_date,
-    s1.people
+    distinct s1.*
 from
     Stadium s1,
     Stadium s2,
     Stadium s3
 where
-    s1.id + 2 = s3.id and
-    s2.id + 1 = s3.id and
+    ((s1.id + 1 = s2.id and s2.id + 1 = s3.id and s3.id - 2 = s1.id) or
+    (s2.id + 1 = s3.id and s3.id + 1 = s1.id and s1.id - 2 = s2.id) or
+    (s3.id + 1 = s1.id and s1.id + 1 = s2.id and s2.id - 2 = s3.id)) and
     s1.people >= 100 and
     s2.people >= 100 and
     s3.people >= 100
-union
-select
-    s2.id,
-    s2.visit_date,
-    s2.people
-from
-    Stadium s1,
-    Stadium s2,
-    Stadium s3
-where
-    s1.id + 2 = s3.id and
-    s2.id + 1 = s3.id and
-    s1.people >= 100 and
-    s2.people >= 100 and
-    s3.people >= 100
-union
-select
-    s3.id,
-    s3.visit_date,
-    s3.people
-from
-    Stadium s1,
-    Stadium s2,
-    Stadium s3
-where
-    s1.id + 2 = s3.id and
-    s2.id + 1 = s3.id and
-    s1.people >= 100 and
-    s2.people >= 100 and
-    s3.people >= 100
-) t
 order by
-    t.visit_date
+    s1.visit_date
 ;
