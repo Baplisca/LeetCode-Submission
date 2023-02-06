732b9bfd010291dce82082dc1ccb628a88f252e9
Baplisca <yokota0327@gmail.com>
2023-02-06 23:13:17 +0900

    add comment

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -6,16 +6,17 @@ public:
         int n = nums.size();
         int ma = n - 1;
         int mi = 0;
+        // corner case
         if (nums[0] < nums[n - 1])
             return nums[0];
         while (ma - mi > 1)
         {
-            cout << ma << " " << mi << " " << endl;
             int mid = (ma + mi) / 2;
+            // Target must be less than or equal to mid index
             if (nums[ma] > nums[mid])
                 ma = mid;
             else
-                mi = mid;
+                mi = mid; // Target must between mid and ma index
         }
         return nums[ma];
     }
